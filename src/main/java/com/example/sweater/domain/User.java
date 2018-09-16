08328bcd7ddd7a901c0da5b2c3b1326221ad8745
Dmitriy <dmitriy.ermakov@is.uz>
2018-09-16 23:19:49 +0600

    Spring Boot Security: добавляем регистрацию и авторизацию пользователей в приложение (простой сайт)

---
+++ src/main/java/com/example/sweater/domain/User.java
@@ -0,0 +1,60 @@
+package com.example.sweater.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name="usr")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String userName;
+    private String password;
+    private boolean isActive;
+
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "userRole", joinColumns = @JoinColumn(name = "userId"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+}
