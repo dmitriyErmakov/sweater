b78faec8dc0640b0b6cd32ca7ef004c969c04843
Dmitriy <dmitriy.ermakov@is.uz>
2018-09-18 22:11:31 +0600

    @Qualifier("dataSource")

--- src/main/java/com/example/sweater/config/WebSecurityConfig.java
+++ src/main/java/com/example/sweater/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.sweater.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,6 +15,7 @@ import javax.sql.DataSource;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
+    @Qualifier("dataSource")
     private DataSource dataSource;
 
     @Override
